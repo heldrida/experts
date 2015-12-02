2f8eb3cf15d5ea9229b3f68e92530065700c383e
Helder <helder@lamoulade.com>
2015-12-02 16:28:02 +0000

    desktop module added to repo

---
+++ desktop-module.php
@@ -0,0 +1,57 @@
+<!-- start: desktop -->
+<div class="desktop">
+
+	<ul class="experts-list">
+		<li>
+			<div class="clip-photo">
+				<img src="data/person01.jpg?201511301834" alt="">
+			</div>
+			<div class="clip-icon">
+				<img src="img/icon-weight-dark.png?201511301824" alt="" class="icon">
+			</div>
+			<h3>Aican <span>Susca</span></h3>
+		</li>
+
+		<li>
+			<div class="clip-photo">
+				<img src="data/person03.jpg?201511301834" alt="">
+			</div>
+			<div class="clip-icon">
+				<img src="img/icon-ball-dark.png?201511301824" alt="" class="icon">
+			</div>
+			<h3>Taviana <span>Naguaran</span></h3>
+		</li>
+
+		<li>
+			<div class="clip-photo">
+				<img src="data/person07.jpg?201511301834" alt="">
+			</div>
+			<div class="clip-icon">
+				<img src="img/icon-glove-dark.png?201511301824" alt="" class="icon">
+			</div>
+			<h3>Chiske <span>Odisglaw</span></h3>
+		</li>
+
+		<li>
+			<div class="clip-photo">
+				<img src="data/person10.jpg?201511301834" alt="">
+			</div>
+			<div class="clip-icon">
+				<img src="img/icon-ball-dark.png?201511301824" alt="" class="icon">
+			</div>
+			<h3>Anne <span>Poposca</span></h3>
+		</li>
+
+		<li>
+			<div class="clip-photo">
+				<img src="data/person09.jpg?201511301834" alt="">
+			</div>
+			<div class="clip-icon">
+				<img src="img/icon-football-dark.png?201511301824" alt="" class="icon">
+			</div>
+			<h3>Duras <span>Catlan</span></h3>
+		</li>
+	</ul>
+
+</div>
+<!-- end: desktop -->
