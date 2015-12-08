1a6b8acc46f66ca38c673d2fb1774bdfb3e4e170
Helder <helder@lamoulade.com>
2015-12-08 13:05:41 +0000

    the quote module was not on the repo

---
+++ quote-module.php
@@ -0,0 +1,4 @@
+<div id="quoteWrp" class="desktop">
+	<p>Dolor sit amet, consectetur adipisicing elit. Abus adipisicing foliarianapoli sta custa.</p>
+	<span>Foo <span>bar</span> | Cyclist</span>
+</div>
