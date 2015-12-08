6f91f96b4881b64e46b359fa35f6be98d2b4407f
Helder <helder@lamoulade.com>
2015-12-08 11:58:05 +0000

    fix for pointer tip, on win resize. created a new wrapper to fix the width, to help position this element absolutely

--- desktop-module.php
+++ desktop-module.php
@@ -3,189 +3,201 @@
 
 	<ul class="experts-list">
 		<li>
-			<?php
+			<div class="expert-wrp">
+				<?php
 
-				$quote = "Pinctio impedit nihia dolores omnis perspiciatis - Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio impedit nihia dolores omnis perspiciatis?";
-				$first_name = "Aican";
-				$second_name = "Susca";
-				$skill = "Skateboarder";
+					$quote = "Pinctio impedit nihia dolores omnis perspiciatis - Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio impedit nihia dolores omnis perspiciatis?";
+					$first_name = "Aican";
+					$second_name = "Susca";
+					$skill = "Skateboarder";
 
-			?>
+				?>
 
-			<div class="clip-photo">
-				<img src="data/person01.jpg?201511301834" alt="">
-			</div>
-			<div class="clip-icon">
-				<img src="img/icon-weight-dark.png?201511301824" alt="" class="icon dark">
-				<img src="img/icon-weight-light.png?201511301824" alt="" class="icon light">
-			</div>
-			<h3><?php echo $first_name; ?> <span><?php echo $second_name; ?></span></h3>
-			<div class="pointer-wrp">
-				<div class="pointer">
-					<span class="tip"></span>
-					<span class="line"></span>
+				<div class="clip-photo">
+					<img src="data/person01.jpg?201511301834" alt="">
 				</div>
-			</div>
+				<div class="clip-icon">
+					<img src="img/icon-weight-dark.png?201511301824" alt="" class="icon dark">
+					<img src="img/icon-weight-light.png?201511301824" alt="" class="icon light">
+				</div>
+				<h3><?php echo $first_name; ?> <span><?php echo $second_name; ?></span></h3>
+				<div class="pointer-wrp">
+					<div class="pointer">
+						<span class="tip"></span>
+						<span class="line"></span>
+					</div>
+				</div>
 
-			<textarea name="quote"><?php echo $quote; ?></textarea>
-			<input type="text" name="first_name" value="<?php echo $first_name; ?>">
-			<input type="text" name="second_name" value="<?php echo $second_name; ?>">
-			<input type="text" name="skill" value="<?php echo $skill; ?>">
+				<textarea name="quote"><?php echo $quote; ?></textarea>
+				<input type="text" name="first_name" value="<?php echo $first_name; ?>">
+				<input type="text" name="second_name" value="<?php echo $second_name; ?>">
+				<input type="text" name="skill" value="<?php echo $skill; ?>">
+			</div>
 		</li>
 
 		<li>
-			<?php
+			<div class="expert-wrp">
+				<?php
 
-				$quote = "Sasit amet, consectetur adipisicing elit. Distinctio impedit nihia dolores omnis perspiciatis?";
-				$first_name = "Taviana";
-				$second_name = "Naguaran";
-				$skill = "Surfer";
+					$quote = "Sasit amet, consectetur adipisicing elit. Distinctio impedit nihia dolores omnis perspiciatis?";
+					$first_name = "Taviana";
+					$second_name = "Naguaran";
+					$skill = "Surfer";
 
-			?>
+				?>
 
-			<div class="clip-photo">
-				<img src="data/person03.jpg?201511301834" alt="">
-			</div>
-			<div class="clip-icon">
-				<img src="img/icon-ball-dark.png?201511301824" alt="" class="icon dark">
-				<img src="img/icon-ball-light.png?201511301824" alt="" class="icon light">
-			</div>
-			<h3><?php echo $first_name; ?> <span><?php echo $second_name; ?></span></h3>
-			<div class="pointer-wrp">
-				<div class="pointer">
-					<span class="tip"></span>
-					<span class="line"></span>
+				<div class="clip-photo">
+					<img src="data/person03.jpg?201511301834" alt="">
 				</div>
-			</div>
+				<div class="clip-icon">
+					<img src="img/icon-ball-dark.png?201511301824" alt="" class="icon dark">
+					<img src="img/icon-ball-light.png?201511301824" alt="" class="icon light">
+				</div>
+				<h3><?php echo $first_name; ?> <span><?php echo $second_name; ?></span></h3>
+				<div class="pointer-wrp">
+					<div class="pointer">
+						<span class="tip"></span>
+						<span class="line"></span>
+					</div>
+				</div>
 
-			<textarea name="quote"><?php echo $quote; ?></textarea>
-			<input type="text" name="first_name" value="<?php echo $first_name; ?>">
-			<input type="text" name="second_name" value="<?php echo $second_name; ?>">
-			<input type="text" name="skill" value="<?php echo $skill; ?>">
+				<textarea name="quote"><?php echo $quote; ?></textarea>
+				<input type="text" name="first_name" value="<?php echo $first_name; ?>">
+				<input type="text" name="second_name" value="<?php echo $second_name; ?>">
+				<input type="text" name="skill" value="<?php echo $skill; ?>">
+			</div>
 		</li>
 
 		<li>
-			<?php
+			<div class="expert-wrp">
+				<?php
 
-				$quote = "Queena uiopo sasit amet, consectetur adipisicing elit. Distinctio impedit nihia dolores omnis perspiciatis caliano posta coolia.";
-				$first_name = "Chiske";
-				$second_name = "Odisglaw";
-				$skill = "Surfer";
+					$quote = "Queena uiopo sasit amet, consectetur adipisicing elit. Distinctio impedit nihia dolores omnis perspiciatis caliano posta coolia.";
+					$first_name = "Chiske";
+					$second_name = "Odisglaw";
+					$skill = "Surfer";
 
-			?>
+				?>
 
-			<div class="clip-photo">
-				<img src="data/person07.jpg?201511301834" alt="">
-			</div>
-			<div class="clip-icon">
-				<img src="img/icon-glove-dark.png?201511301824" alt="" class="icon dark">
-				<img src="img/icon-glove-light.png?201511301824" alt="" class="icon light">
-			</div>
-			<h3><?php echo $first_name; ?> <span><?php echo $second_name; ?></span></h3>
-			<div class="pointer-wrp">
-				<div class="pointer">
-					<span class="tip"></span>
-					<span class="line"></span>
+				<div class="clip-photo">
+					<img src="data/person07.jpg?201511301834" alt="">
 				</div>
-			</div>
+				<div class="clip-icon">
+					<img src="img/icon-glove-dark.png?201511301824" alt="" class="icon dark">
+					<img src="img/icon-glove-light.png?201511301824" alt="" class="icon light">
+				</div>
+				<h3><?php echo $first_name; ?> <span><?php echo $second_name; ?></span></h3>
+				<div class="pointer-wrp">
+					<div class="pointer">
+						<span class="tip"></span>
+						<span class="line"></span>
+					</div>
+				</div>
 
-			<textarea name="quote"><?php echo $quote; ?></textarea>
-			<input type="text" name="first_name" value="<?php echo $first_name; ?>">
-			<input type="text" name="second_name" value="<?php echo $second_name; ?>">
-			<input type="text" name="skill" value="<?php echo $skill; ?>">
+				<textarea name="quote"><?php echo $quote; ?></textarea>
+				<input type="text" name="first_name" value="<?php echo $first_name; ?>">
+				<input type="text" name="second_name" value="<?php echo $second_name; ?>">
+				<input type="text" name="skill" value="<?php echo $skill; ?>">
+			</div>
 		</li>
 
 		<li>
-			<?php
+			<div class="expert-wrp">
+				<?php
 
-				$quote = "Queena uiopo sasit amet, consectetur adipisicing elit. Distinctio impedit nihia dolores omnis perspiciatis caliano posta coolia.";
-				$first_name = "Anne";
-				$second_name = "Poposca";
-				$skill = "Snowboarder";
+					$quote = "Queena uiopo sasit amet, consectetur adipisicing elit. Distinctio impedit nihia dolores omnis perspiciatis caliano posta coolia.";
+					$first_name = "Anne";
+					$second_name = "Poposca";
+					$skill = "Snowboarder";
 
-			?>
+				?>
 
-			<div class="clip-photo">
-				<img src="data/person10.jpg?201511301834" alt="">
-			</div>
-			<div class="clip-icon">
-				<img src="img/icon-ball-dark.png?201511301824" alt="" class="icon dark">
-				<img src="img/icon-ball-light.png?201511301824" alt="" class="icon light">
-			</div>
-			<h3><?php echo $first_name; ?> <span><?php echo $second_name; ?></span></h3>
-			<div class="pointer-wrp">
-				<div class="pointer">
-					<span class="tip"></span>
-					<span class="line"></span>
+				<div class="clip-photo">
+					<img src="data/person10.jpg?201511301834" alt="">
 				</div>
-			</div>
+				<div class="clip-icon">
+					<img src="img/icon-ball-dark.png?201511301824" alt="" class="icon dark">
+					<img src="img/icon-ball-light.png?201511301824" alt="" class="icon light">
+				</div>
+				<h3><?php echo $first_name; ?> <span><?php echo $second_name; ?></span></h3>
+				<div class="pointer-wrp">
+					<div class="pointer">
+						<span class="tip"></span>
+						<span class="line"></span>
+					</div>
+				</div>
 
-			<textarea name="quote"><?php echo $quote; ?></textarea>
-			<input type="text" name="first_name" value="<?php echo $first_name; ?>">
-			<input type="text" name="second_name" value="<?php echo $second_name; ?>">
-			<input type="text" name="skill" value="<?php echo $skill; ?>">
+				<textarea name="quote"><?php echo $quote; ?></textarea>
+				<input type="text" name="first_name" value="<?php echo $first_name; ?>">
+				<input type="text" name="second_name" value="<?php echo $second_name; ?>">
+				<input type="text" name="skill" value="<?php echo $skill; ?>">
+			</div>
 		</li>
 
 		<li>
-			<?php
+			<div class="expert-wrp">
+				<?php
 
-				$quote = "Queena uiopo sasit amet, consectetur adipisicing elit. Distinctio impedit nihia dolores omnis perspiciatis caliano posta coolia.";
-				$first_name = "Duras";
-				$second_name = "Catlan";
-				$skill = "Bmx rider";
+					$quote = "Queena uiopo sasit amet, consectetur adipisicing elit. Distinctio impedit nihia dolores omnis perspiciatis caliano posta coolia.";
+					$first_name = "Duras";
+					$second_name = "Catlan";
+					$skill = "Bmx rider";
 
-			?>
+				?>
 
-			<div class="clip-photo">
-				<img src="data/person09.jpg?201511301834" alt="">
-			</div>
-			<div class="clip-icon">
-				<img src="img/icon-football-dark.png?201511301824" alt="" class="icon dark">
-				<img src="img/icon-football-light.png?201511301824" alt="" class="icon light">
-			</div>
-			<h3><?php echo $first_name; ?> <span><?php echo $second_name; ?></span></h3>
-			<div class="pointer-wrp">
-				<div class="pointer">
-					<span class="tip"></span>
-					<span class="line"></span>
+				<div class="clip-photo">
+					<img src="data/person09.jpg?201511301834" alt="">
 				</div>
-			</div>
+				<div class="clip-icon">
+					<img src="img/icon-football-dark.png?201511301824" alt="" class="icon dark">
+					<img src="img/icon-football-light.png?201511301824" alt="" class="icon light">
+				</div>
+				<h3><?php echo $first_name; ?> <span><?php echo $second_name; ?></span></h3>
+				<div class="pointer-wrp">
+					<div class="pointer">
+						<span class="tip"></span>
+						<span class="line"></span>
+					</div>
+				</div>
 
-			<textarea name="quote"><?php echo $quote; ?></textarea>
-			<input type="text" name="first_name" value="<?php echo $first_name; ?>">
-			<input type="text" name="second_name" value="<?php echo $second_name; ?>">
-			<input type="text" name="skill" value="<?php echo $skill; ?>">
+				<textarea name="quote"><?php echo $quote; ?></textarea>
+				<input type="text" name="first_name" value="<?php echo $first_name; ?>">
+				<input type="text" name="second_name" value="<?php echo $second_name; ?>">
+				<input type="text" name="skill" value="<?php echo $skill; ?>">
+			</div>
 		</li>
 
 		<li>
-			<?php
+			<div class="expert-wrp">
+				<?php
 
-				$quote = "Queena uiopo sasit amet, consectetur adipisicing elit. Distinctio impedit nihia dolores omnis perspiciatis caliano posta coolia.";
-				$first_name = "Orion";
-				$second_name = "Massai";
-				$skill = "Skydiver";
+					$quote = "Queena uiopo sasit amet, consectetur adipisicing elit. Distinctio impedit nihia dolores omnis perspiciatis caliano posta coolia.";
+					$first_name = "Orion";
+					$second_name = "Massai";
+					$skill = "Skydiver";
 
-			?>
+				?>
 
-			<div class="clip-photo">
-				<img src="data/person05.jpg?201511301834" alt="">
-			</div>
-			<div class="clip-icon">
-				<img src="img/icon-football-dark.png?201511301824" alt="" class="icon dark">
-				<img src="img/icon-football-light.png?201511301824" alt="" class="icon light">
-			</div>
-			<h3><?php echo $first_name; ?> <span><?php echo $second_name; ?></span></h3>
-			<div class="pointer-wrp">
-				<div class="pointer">
-					<span class="tip"></span>
-					<span class="line"></span>
+				<div class="clip-photo">
+					<img src="data/person05.jpg?201511301834" alt="">
 				</div>
-			</div>
+				<div class="clip-icon">
+					<img src="img/icon-football-dark.png?201511301824" alt="" class="icon dark">
+					<img src="img/icon-football-light.png?201511301824" alt="" class="icon light">
+				</div>
+				<h3><?php echo $first_name; ?> <span><?php echo $second_name; ?></span></h3>
+				<div class="pointer-wrp">
+					<div class="pointer">
+						<span class="tip"></span>
+						<span class="line"></span>
+					</div>
+				</div>
 
-			<textarea name="quote"><?php echo $quote; ?></textarea>
-			<input type="text" name="first_name" value="<?php echo $first_name; ?>">
-			<input type="text" name="second_name" value="<?php echo $second_name; ?>">
-			<input type="text" name="skill" value="<?php echo $skill; ?>">
+				<textarea name="quote"><?php echo $quote; ?></textarea>
+				<input type="text" name="first_name" value="<?php echo $first_name; ?>">
+				<input type="text" name="second_name" value="<?php echo $second_name; ?>">
+				<input type="text" name="skill" value="<?php echo $skill; ?>">
+			</div>
 		</li>
 
 	</ul>
